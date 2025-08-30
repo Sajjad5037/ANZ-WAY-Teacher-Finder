843040c53999febc5345755b351948caa272e88b
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-08-31 04:23:14 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"], // scans all components and pages
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
