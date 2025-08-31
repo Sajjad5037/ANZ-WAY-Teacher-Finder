de1f0c68e0bc290696541e4d3b8ac98ab7771990
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-08-31 11:57:19 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -105,40 +105,67 @@ export default function Navbar() {
 
       {/* Mobile Menu */}
       {isOpen && (
-        <ul className="md:hidden bg-white px-2 pt-2 pb-3 space-y-1 shadow-md">
-          <li>
-            <Link
-              to="/"
-              className="block px-3 py-2 rounded hover:bg-gray-100"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/courses"
-              className="block px-3 py-2 rounded hover:bg-gray-100"
-            >
-              Courses
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/about"
-              className="block px-3 py-2 rounded hover:bg-gray-100"
-            >
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact"
-              className="block px-3 py-2 rounded hover:bg-gray-100"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
+        <ul className="hidden md:flex space-x-6 items-center">
+  <li>
+    <Link
+      to="/"
+      className="px-3 py-2 rounded hover:text-primary-dark font-bold"
+    >
+      Home
+    </Link>
+  </li>
+  <li>
+    <Link
+      to="/courses"
+      className="px-3 py-2 rounded hover:text-primary-dark font-bold"
+    >
+      Courses
+    </Link>
+  </li>
+  <li className="relative group flex items-center">
+    <Link
+      to="/about"
+      className="px-3 py-2 rounded hover:text-primary-dark flex items-center font-bold"
+    >
+      About Us
+      <svg className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20">
+        <path
+          fillRule="evenodd"
+          d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.08 1.04l-4.25 4.25a.75.75 0 01-1.08 0L5.21 8.27a.75.75 0 01.02-1.06z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </Link>
+    {/* Dropdown */}
+    <ul className="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[150px] z-10">
+      <li>
+        <Link
+          to="/about/team"
+          className="block px-4 py-2 hover:bg-gray-100"
+        >
+          Our Team
+        </Link>
+      </li>
+      <li>
+        <Link
+          to="/about/mission"
+          className="block px-4 py-2 hover:bg-gray-100"
+        >
+          Our Mission
+        </Link>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <Link
+      to="/contact"
+      className="px-3 py-2 rounded hover:text-primary-dark font-bold"
+    >
+      Contact
+    </Link>
+  </li>
+</ul>
+
       )}
     </nav>
   );
