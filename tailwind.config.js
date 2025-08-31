6e0b23f833f10aefc1af06ecccd78f78aea3c32f
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-08-31 05:06:33 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#9b51e0', // Homebridge purple
-        secondary: '#ffffff', // white or accent
+        primary: '#9b51e0',      // Homebridge purple
+        primary-dark: '#872dcf', // slightly darker for hover
+        secondary: '#ffffff',     // white for text/icons
       },
     },
   },
-};
+  plugins: [],
+}
