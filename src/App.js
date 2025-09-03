c5d35260e019a2dde149a818d7d98a4d5fb8e552
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-09-03 12:19:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,31 +3,39 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import Courses from './pages/Courses';
+import Teachers from './pages/Teachers';
+import TeacherRegistration from './pages/TeacherRegistration';
+import TeacherLogin from './pages/TeacherLogin';
+import StudentRegistration from './pages/StudentRegistration';
+import StudentLogin from './pages/StudentLogin';
+import Dashboard from './pages/Dashboard';
 import About from './pages/About';
 import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
-      
-      {/* Navbar with white background */}
+      {/* Navbar */}
       <div className="bg-white text-primary">
         <Navbar />
       </div>
 
-
-      {/* Main content area with light purple background */}
+      {/* Main Content */}
       <div className="bg-lightPurple min-h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/courses" element={<Courses />} />
+          <Route path="/teachers" element={<Teachers />} />                  {/* Browse teachers */}
+          <Route path="/register-teacher" element={<TeacherRegistration />} />
+          <Route path="/teacher-login" element={<TeacherLogin />} />
+          <Route path="/register-student" element={<StudentRegistration />} /> {/* New student signup */}
+          <Route path="/student-login" element={<StudentLogin />} />         {/* New student login */}
+          <Route path="/dashboard" element={<Dashboard />} />               {/* Dashboard for logged-in users */}
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
 
-      {/* Footer with dark purple background */}
+      {/* Footer */}
       <div className="bg-secondary text-white">
         <Footer />
       </div>
